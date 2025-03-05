2cff42e85f5fcf58001ea363d702731fcccfa2a2
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-03-05 13:40:17 -0800

    fix embeds in frames

--- src/app/creators/[username]/page.tsx
+++ src/app/creators/[username]/page.tsx
@@ -15,3 +15,39 @@ export default async function ProfilePage({ params }: Props) {
     </div>
   );
 }
+
+import { Metadata } from "next";
+
+const appUrl = `https://${process.env.VERCEL_URL}`;
+// const appUrl = process.env.NEXT_PUBLIC_URL;
+// const appUrl = process.env.VERCEL_URL;
+
+const frame = {
+  version: "next",
+  imageUrl: `${appUrl}/opengraph-image`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Join Frame",
+      url: `${appUrl}/join`,
+      splashImageUrl: `${appUrl}/splash.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Join Frame",
+    openGraph: {
+      title: "Join Frame",
+      description: "Join the creatordirectory.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
--- src/app/creators/page.tsx
+++ src/app/creators/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Metadata } from "next";
 
 import { useEffect, useState } from "react";
 import sdk, { type Context } from "@farcaster/frame-sdk";
@@ -132,4 +133,38 @@ export default function DirectoryPage() {
       </div>
     </div>
   );
+}
+
+// const appUrl = process.env.NEXT_PUBLIC_URL;
+// const appUrl = process.env.VERCEL_URL;
+const appUrl = `https://${process.env.VERCEL_URL}`;
+
+const frame = {
+  version: "next",
+  imageUrl: `${appUrl}/opengraph-image`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Creator Directory",
+      url: `${appUrl}/creators`,
+      splashImageUrl: `${appUrl}/splash.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Creator Directory",
+    openGraph: {
+      title: "Creator Directory",
+      description: "The creator directory.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
 }
