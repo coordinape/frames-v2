b4298b0995449d5b9c522a5bafda41fd8c5d1c01
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-03-05 13:40:17 -0800

    fix embeds in frames

--- src/app/(auth)/join/page.tsx
+++ src/app/(auth)/join/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import sdk, { type Context } from "@farcaster/frame-sdk";
+import { Metadata } from "next";
 
 export default function JoinPage() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
@@ -110,4 +111,38 @@ export default function JoinPage() {
       </div>
     </div>
   );
+}
+
+// const appUrl = process.env.NEXT_PUBLIC_URL;
+// const appUrl = process.env.VERCEL_URL;
+const appUrl = `https://${process.env.VERCEL_URL}`;
+
+const frame = {
+  version: "next",
+  imageUrl: `${appUrl}/opengraph-image`,
+  button: {
+    title: "Launch Frame",
+    action: {
+      type: "launch_frame",
+      name: "Join Frame",
+      url: `${appUrl}/join`,
+      splashImageUrl: `${appUrl}/splash.png`,
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Join Frame",
+    openGraph: {
+      title: "Join Frame",
+      description: "Join the creatordirectory.",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
 }
