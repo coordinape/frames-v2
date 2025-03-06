611cd5699a28a034c7319a888acea5ac4496b338
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-03-06 12:58:40 -0800

    Fix next auth errors

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import type { Session } from "next-auth"
-import { SessionProvider } from "next-auth/react"
-
+import type { Session } from "next-auth";
+// import { SessionProvider } from "next-auth/react";
 
 const WagmiProvider = dynamic(
   () => import("~/components/providers/WagmiProvider"),
@@ -12,12 +11,16 @@ const WagmiProvider = dynamic(
   }
 );
 
-export function Providers({ session, children }: { session: Session | null, children: React.ReactNode }) {
+export function Providers({
+  session,
+  children,
+}: {
+  session: Session | null;
+  children: React.ReactNode;
+}) {
   return (
-    <SessionProvider session={session}>
-      <WagmiProvider>
-        {children}
-      </WagmiProvider>
-    </SessionProvider>
+    // <SessionProvider session={session}>
+    <WagmiProvider>{children}</WagmiProvider>
+    // </SessionProvider>
   );
 }
