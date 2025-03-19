6d1d425c676bffd654f64877050d53fbef020821
Wing <wingfeatherwave@gmail.com>
2025-03-19 16:50:23 -0700

    adding width and height

--- src/app/ogimage/route.tsx
+++ src/app/ogimage/route.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/og";
+import { baseBlue, basePixelLow, IMAGE_URL_BASE } from "./helpers";
 import { NextRequest } from "next/server";
 
 // export const runtime = 'edge'; // Add this line to use the Edge runtime
@@ -16,18 +17,20 @@ export async function GET(request: NextRequest) {
       <div
         tw="h-full w-full flex flex-col justify-between items-center relative text-white"
         style={{
-          backgroundColor: "#abc123", // This is the bg-base-blue color
-          // fontFamily: "BasePixel-Low",
-          // letterSpacing: "0.02em",
+          backgroundColor: baseBlue, // This is the bg-base-blue color
+          fontFamily: "BasePixel-Low",
+          letterSpacing: "0.02em",
         }}
       >
         <h1 tw="text-2xl">{title}</h1>
         <div tw="flex items-center justify-center flex-1">
-          {/* <img
+          <img
             src={IMAGE_URL_BASE + "Base_Symbol_White.png"}
             tw="h-38 mr-5"
+            width={138}
+            height={138}
             alt="BASE Logo"
-          /> */}
+          />
           <div tw="flex flex-col justify-start">
             <p tw="text-5xl mt-0 mb-1">Join</p>
             <p tw="text-5xl mt-0 mb-1">The Creators</p>
@@ -42,14 +45,14 @@ export async function GET(request: NextRequest) {
         "Content-Type": "image/png",
       },
       // Include the font in the response
-      // fonts: [
-      //   {
-      //     name: "BasePixel-Low",
-      //     data: basePixelLow,
-      //     style: "normal",
-      //     weight: 400,
-      //   },
-      // ],
+      fonts: [
+        {
+          name: "BasePixel-Low",
+          data: basePixelLow,
+          style: "normal",
+          weight: 400,
+        },
+      ],
     },
   );
 }
