429b096916f7ade8c2c12f34a515e74064ce1667
graffe <graffe@graffe.co>
2025-03-20 10:08:27 -0700

    cleanup search

--- src/app/creators/CreatorsList.tsx
+++ src/app/creators/CreatorsList.tsx
@@ -143,25 +143,12 @@ export default function CreatorsList() {
           </svg>
           <input
             type="text"
-            placeholder="Name, Skill, Medium, Location..."
+            placeholder="Name, Medium, etc..."
             className="w-full bg-transparent border-none text-white py-3 px-2 focus:outline-none"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
-          <svg
-            className="w-5 h-5 text-white"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M19 9l-7 7-7-7"
-            ></path>
-          </svg>
+          
         </div>
       </div>
 
