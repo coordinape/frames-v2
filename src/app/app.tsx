4a1c493a75a458ad136c4a2acee607d4597c1548
crabsinger <83605543+crabsinger@users.noreply.github.com>
2025-03-20 16:15:19 -0700

    redirect root to /creators

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import dynamic from "next/dynamic";
+import { redirect } from "next/navigation";
 
-const Demo = dynamic(() => import("~/components/Demo"), {
-  ssr: false,
-});
-
-export default function App(
-  { title }: { title?: string } = { title: "Frames v2 Demo" },
-) {
-  return <Demo title={title} />;
+export default function App() {
+  redirect("/creators");
 }
