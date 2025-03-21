c43d29f858408071a50f24999cb742aa2855f251
graffe <graffe@graffe.co>
2025-03-20 17:23:19 -0700

    restore injected

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -14,8 +14,7 @@ export const config = createConfig({
     [degen.id]: http(),
     [unichain.id]: http(),
   },
-  // connectors: [injected(), farcasterFrame()],
-  connectors: [farcasterFrame()],
+  connectors: [injected(), farcasterFrame()],
 });
 
 const queryClient = new QueryClient();
