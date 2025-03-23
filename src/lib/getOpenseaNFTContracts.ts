0b1e9fd8ac74dbd6d89f0a81f93288037a4c121c
graffe <graffe@graffe.co>
2025-03-23 12:44:27 -0400

    unifying zapper/opensea contract checking

--- src/lib/getOpenseaNFTContracts.ts
+++ src/lib/getOpenseaNFTContracts.ts
@@ -96,17 +96,17 @@ interface OpenSeaCollection {
   }[];
 }
 
-interface ContractDetails {
+export interface ContractDetails {
   name: string;
   contractAddress: string;
   chainId: string;
   imageUrl: string;
   bannerImageUrl: string;
   description: string;
-  openseaUrl: string;
-  projectUrl: string;
-  discordUrl: string;
-  twitterUsername: string;
+  openseaUrl?: string;
+  projectUrl?: string;
+  discordUrl?: string;
+  twitterUsername?: string;
 }
 
 // Add a function to filter collections by chain
