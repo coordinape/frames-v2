ca39bd2aa190a2c7ca6473a72c32cad59340a92f
graffe <graffe@graffe.co>
2025-04-08 14:53:35 -0700

    fix titles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import "~/app/globals.css";
 
 export const metadata: Metadata = {
-  title: "Farcaster Frames v2 Demo",
-  description: "A Farcaster Frames v2 demo app",
+  title: "Based Creators Directory",
+  description: "Based Creators Directory",
   icons: {
     icon: "/favicon.png", // This will be your new favicon
   },
